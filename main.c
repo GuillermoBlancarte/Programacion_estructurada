2f66ee9f834830d527b8e9d33d5a3d06fd753c7d
Guillermo Blancarte <guillermooblancarte@gmail.com>
2020-02-02 16:12:57 -0600

    Actividad1

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-/*Blancarte Cordero Guillermo  Matrcua:1843282  Gpo:016*/
+/*Blancarte Cordero Guillermo  Matricula:1843282  Gpo:016*  Horario:Lunes 8:00pm-10:00pm */ 
 
 int main(int argc, char *argv[]) {
 	printf("Hola mundo. 1843282\n");
