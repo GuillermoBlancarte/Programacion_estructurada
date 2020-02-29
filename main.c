32cc01d03e5a36cd4720ebf40686acdafc7460e5
Guillermo Blancarte <guillermooblancarte@gmail.com>
2020-02-29 15:56:22 -0600

    ifelse

--- main.c
+++ main.c
@@ -1,9 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+//Blancarte Cordero Guillermo Gpo:016  Matricula:1843282  Horario:Lunes 8pm-10pm  fecha:17-feb-2020//
 
 int main(int argc, char *argv[]) {
-	printf("Hola mundo. 1843282\n");
+	char letra =0;
+	printf("Oprima una tecla:");
+	scanf(" %c",&letra);
+	
+	if (letra>=48 && letra<=57){
+		printf("Su caracter es un entero\n");
+        printf("Numero en el codigo ascci:%d\n", letra);
+	}else
+	    if (letra>=65 && letra<=90){
+	    	printf("Su caracter es una letra mayuscula\n");
+            printf("Numero en el codigo ascci:%d\n", letra);
+		}else
+		    if (letra>=97 && letra<=122){
+		    	printf("Su caracter es una letra minuscula\n");
+                printf("Numero en el codigo ascci:%d\n", letra);
+			}
 	return 0;
 }
