9818829aeae1a4fd6b43bd833dd73fcc7fff6349
Guillermo Blancarte <guillermooblancarte@gmail.com>
2020-03-14 23:40:27 -0600

    Do_while

--- main.c
+++ main.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+//BLANCARTE CORDERO GUILLERMO   MATRICULA:1843282  GPO:016 HORARIO:LUNES 8PM-10PM  FECHA:2-MARZO-2020//
 
 int main(int argc, char *argv[]) {
-	printf("Hola mundo. 1843282\n");
+	int num=0,i=0,suma=0,prom=0;
+do{
+	printf("Ingrese un numero positivo o si desea terminar ingrese un numero negativo:");
+	scanf("%d",&num);
+	suma= suma+num;
+	i=i+1;
+}	while(num>=0);
+
+    prom=suma/i;
+    printf("\nEl promedio de los numeros ingresado es:%d",prom);
+    
 	return 0;
 }
