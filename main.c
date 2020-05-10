a815e324fb6fda790e2efe4bc5467bd0ee15d5e6
Guillermo Blancarte <guillermooblancarte@gmail.com>
2020-05-10 17:20:30 -0500

    Matrices

--- main.c
+++ main.c
@@ -7,23 +7,23 @@
 
 int main(int argc, char *argv[]) {
 	
-	int x,num=0,cont=0,z=0,i=0,tabla[100];
-	printf("Ingrese un numero menor o igual a 100:");
-	scanf("%d",&num);
-	for(x=1;x<=num;x++){
-		cont=0;
-		for(z=1;z<=x;z++){
-		    if(x%z==0){
-		    	cont++;
-			}	
+    int a[4][4],i,j,esc=0;
+	printf("\tMATRIZ DE [3X3]\n\n");
+	for(i=1;i<=3;i++){
+		for(j=1;j<=3;j++){
+			printf("Introducir el valor de [%d][%d]:",i,j);
+			scanf("%d",&a[i][j]);
 		}
-	if(cont==2 || z==1 || z==0){
-		tabla[i]=x;
-		i++;
-	}		
-}
-	for(x=0;x<i;x++){
-		printf("%d\n",tabla[x]);
+	}
+	printf("\n\nIngrese un valor escalar(entero):");
+	scanf("%d",&esc);
+	
+	for(i=1;i<=3;i++){
+		for(j=1;j<=3;j++){
+		    a[i][j]*= esc;
+		    printf("[%d]",a[i][j]);
+		}
+		    printf("\n");
 	}
 	
 	return 0;
